0af2e3ca3ce422363a0707a77cc2d98c59977d6d
Dawid <daw1d99@o2.pl>
2023-12-03 22:44:36 +0100

    Zadanie8SM

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.zadanie8"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.zadanie8"
+        minSdk = 17
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("androidx.navigation:navigation-fragment:2.7.5")
+    implementation("androidx.navigation:navigation-ui:2.7.5")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("androidx.room:room-runtime:2.2.2")
+    annotationProcessor ("androidx.room:room-compiler:2.2.2")
+    androidTestImplementation ("androidx.room:room-testing:2.2.2")
+
+    implementation ("androidx.lifecycle:lifecycle-common-java8:2.1.0")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.1.0")
+
+    implementation("androidx.cardview:cardview:1.0.0") // Use the latest version
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+
+}
+
